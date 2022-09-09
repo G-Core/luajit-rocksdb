b0705945ca902e30442d558f98a97bd6bf9a0d74
Joe Mariadassou <theSundayProgrammer@gmail.com>
2022-09-10 06:13:08 +1000

    adding a test for column families

---
+++ test/test_column_families.lua
@@ -0,0 +1,51 @@
+local rocksdb = require("rocksdb")
+local format = string.format
+
+for k,v in pairs(rocksdb) do
+  print(k..": "..tostring(v))
+end
+
+local options = rocksdb.options({
+  increase_parallelism = 1,
+  create_missing_column_families = true,
+  create_if_missing = true
+})
+
+assert(options.class == "options")
+
+local db = rocksdb.open_with_cf(options, "/tmp/rocksdb2.test", "default", "columna")
+
+local writeoptions = rocksdb.writeoptions()
+assert(writeoptions.class == "writeoptions")
+
+local readoptions = rocksdb.readoptions()
+assert(readoptions.class == "readoptions")
+
+local key, value, expected_value
+
+print("start: put")
+for i = 0, 1000 do
+  key = format("lrocks_db_key:%d", i)
+  value = format("lrocks_db_value:%d", i)
+  db:put_with_cf(writeoptions, 1, key, value)
+end
+print("done: put")
+
+print("start: get")
+for i = 0, 1000 do
+  key = format("lrocks_db_key:%d", i)
+  expected_value = format("lrocks_db_value:%d", i)
+  value = db:get_with_cf(readoptions,1, key)
+  assert(value == expected_value)
+end
+print("done: get")
+db:close()
+print("closed")
+
+collectgarbage()
+
+
+readoptions:destroy()
+writeoptions:destroy()
+options:destroy()
+
